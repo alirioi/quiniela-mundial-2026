d01f8632fd3c3a12b4ef6a518270c1dd4ffab8cf
Alirio Isea <isealirio@gmail.com>
2026-05-23 21:04:37 -0400

    feat: agregar soporte para sitemap y mejorar accesibilidad en componentes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,25 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://quiniela.alirioi.dev',
   output: 'server',
   adapter: netlify(),
-  integrations: [react(), tailwind()]
+  integrations: [
+    react(), 
+    tailwind(), 
+    sitemap({
+      filter: (page) => 
+        !page.includes('/admin') && 
+        !page.includes('/dashboard') &&
+        !page.includes('/pending') &&
+        !page.includes('/my-entries') &&
+        !page.includes('/predictions') &&
+        !page.includes('/reset-password') &&
+        !page.includes('/forgot-password')
+    })
+  ]
 });
